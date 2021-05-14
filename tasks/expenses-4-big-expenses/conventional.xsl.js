3c15b2d0ef9fecb5fd69f1c3f559331c9ed8644e
Marcel Schaeben <m.schaeben@uni-koeln.de>
2021-05-14 16:32:42 +0200

    add expenses 4: big expenses

---
+++ tasks/expenses-4-big-expenses/conventional.xsl.js
@@ -0,0 +1,34 @@
+const { xml } = require("../common/input-data")
+const { outputResult } = require("../common/utils")
+
+function selectBigExpenses(xml) {
+    const exchangeRate = 0.9
+    let matched = []
+    for (let elem of xml.getElementsByTagName("*")) {
+        if (elem.tagName === "expense") {
+            const value = parseInt(elem.getAttribute("value"))
+            let valueInEur;
+
+            if (!elem.hasAttribute("currency") ||
+                elem.getAttribute("currency").toUpperCase() === "EUR")
+            {
+                valueInEur = value
+            } else {
+                valueInEur = exchangeRate
+            }
+
+            if (valueInEur >= 100) {
+                const expenseId = elem.getAttribute("id")
+                matched.push(expenseId)
+            }
+        }
+    }
+
+    const matched_list = matched.join("; ")
+    return matched_list
+}
+
+exports.fn = selectBigExpenses
+
+const result = selectBigExpenses(xml)
+outputResult(module, result)
