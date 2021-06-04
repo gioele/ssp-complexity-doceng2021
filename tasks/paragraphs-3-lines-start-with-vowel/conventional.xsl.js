00402a2eef734da4310b6cd4362219593521dcee
Marcel Schaeben <m.schaeben@uni-koeln.de>
2021-06-04 18:48:14 +0200

    paragraphs-3: add conventional js

---
+++ tasks/paragraphs-3-lines-start-with-vowel/conventional.xsl.js
@@ -0,0 +1,66 @@
+const { xml, precedingNodes, followingNodes } = require("../common/input-data")
+const { outputResult } = require("../common/utils")
+
+function linesStartWithVowel(xml) {
+    const lineNumbersStartingWithVowel = []
+
+    for (let elem of xml.getElementsByTagName("*")) {
+        if (elem.tagName === "line") {
+            const fullWords = []
+
+            for (let child of elem.childNodes) {
+                if (child.tagName === "w") {
+                    let word = child.textContent
+
+
+                    const preceding = precedingNodes(child)
+                    let precedingWordElem = null
+
+                    for (let precedingNode of preceding.reverse()) {
+                        if (precedingNode.tagName === "w") {
+                            precedingWordElem = precedingNode
+                            break
+                        }
+                    }
+
+                    const precedingWord = precedingWordElem?.textContent
+                    if (precedingWord?.endsWith("-")) {
+                        // noop
+                    } else if (word.endsWith("-")) {
+                        let followingWord = ""
+                        const following = followingNodes(child)
+                        for (let followingNode of following) {
+                            if (followingNode.tagName === "w") {
+                                followingWord = followingNode.textContent
+                                break
+                            }
+                        }
+
+                        const normalizedStart = word.replace("-", "")
+                        const joinedWord = normalizedStart + followingWord
+
+                        fullWords.push(joinedWord)
+                    } else {
+                        fullWords.push(word)
+                    }
+                }
+            }
+            const lineText = fullWords.join(" ")
+            if (["a", "e", "i", "o", "u"].includes(lineText.substring(0, 1))) {
+                for (attrib of elem.attributes) {
+                    if (attrib.name === "n") {
+                        lineNumbersStartingWithVowel.push(attrib.value)
+                        break
+                    }
+                }
+            }
+        }
+    }
+
+    return lineNumbersStartingWithVowel.join("; ")
+}
+
+exports.fn = linesStartWithVowel
+
+const result = linesStartWithVowel(xml)
+outputResult(module, result)
