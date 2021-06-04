d253aedbf0ef84e3ed23ef89486a7265696d31a5
Marcel Schaeben <m.schaeben@uni-koeln.de>
2021-06-04 17:21:10 +0200

    add preceding and following axes helpers (missing in DOM api)

--- tasks/common/input-data.js
+++ tasks/common/input-data.js
@@ -1,12 +1,22 @@
 const fs = require("fs")
 const { JSDOM } = require("jsdom")
 
+function xPathResultToNodeArray(xPathResult) {
+    const nodes = [];
+    let node = xPathResult.iterateNext();
+    while (node) {
+        nodes.push(node);
+        node = xPathResult.iterateNext();
+    }
+
+    return nodes
+}
+
 function parseXML(xml_string) {
     const { window } = new JSDOM("<html/>")
     const parser = new window.DOMParser()
     const doc = parser.parseFromString(xml_string, "text/xml")
-    const root = doc.documentElement
-    return root
+    return {window, doc}
 }
 
 const args = process.argv.slice(2)
@@ -16,7 +26,16 @@ if (data_file === "-") {
     data_file = 0 // stdin
 }
 const data = fs.readFileSync(data_file, "utf8").toString()
-const xml = parseXML(data)
+const result = parseXML(data)
 
 exports.data = data
-exports.xml = xml
+exports.xml = result.doc.documentElement
+exports.doc = result.doc
+exports.precedingNodes = function(node) {
+    const xPathResult = result.doc.evaluate("./preceding::*", node, null, result.window.XPathResult.ORDERED_NODE_ITERATOR_TYPE)
+    return xPathResultToNodeArray(xPathResult)
+}
+exports.followingNodes = function(node) {
+    const xPathResult =  result.doc.evaluate("./following::*", node, null, result.window.XPathResult.ORDERED_NODE_ITERATOR_TYPE)
+    return xPathResultToNodeArray(xPathResult)
+}
